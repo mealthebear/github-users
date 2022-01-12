69c6b6f217a67b3d5f4c33f4a6a5de37a11d8afa
mealthebear <mealthebear@gmail.com>
2022-01-11 22:08:09 -0800

    Installed axios and made example fetch call

--- github-users/src/App.js
+++ github-users/src/App.js
@@ -1,6 +1,19 @@
 import './App.css';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 
 const App = () => {
+  const [listOfUsers, setUsers] = useState([]);
+
+  const fetchUsers = async () => {
+    const users = await axios.get('https://api.github.com/search/users?q=example');
+    console.log(users.data.items);
+  }
+
+  useEffect(() => {
+    fetchUsers();
+  })
+
   return (
     <>
       Rendered the app!
