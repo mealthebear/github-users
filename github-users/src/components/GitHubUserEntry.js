479e50fa44b809713810750d86904fcfd3a0e884
mealthebear <mealthebear@gmail.com>
2022-01-12 19:44:16 -0800

    Evenly spaced user profile as well as user stats

--- github-users/src/components/GitHubUserEntry.js
+++ github-users/src/components/GitHubUserEntry.js
@@ -3,15 +3,19 @@ import React from 'react';
 const GitHubUserEntry = ({ userInfo }) => {
   return (
     <div className='user-entry'>
-      <div className='user-name'>
-        {userInfo.username}
+      <div className='user-profile'>
+        <div className='user-name'>
+          {userInfo.username}
+        </div>
+        <div className='user-image'>
+          <a href={userInfo.url}><img className='profile-photo' src={userInfo.image} /></a>
+        </div>
       </div>
-      <div>
-        <a href={userInfo.url}><img className='user-image' src={userInfo.image} /></a>
+      <div className='user-stats'>
+        <div className='user-stat'>Number of repos: {userInfo.repoCount}</div>
+        <div className='user-stat'>Number of stargazers: {userInfo.stargazerCount}</div>
+        <div className='user-stat'>Number of followers: {userInfo.followerCount}</div>
       </div>
-      <div className='user-stat'>Number of repos: {userInfo.repoCount}</div>
-      <div className='user-stat'>Number of stargazers: {userInfo.stargazerCount}</div>
-      <div className='user-stat'>Number of followers: {userInfo.followerCount}</div>
     </div>
   )
 }
