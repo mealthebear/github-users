0e8cc0875ab6b894422af74480dacff6b0790f96
mealthebear <mealthebear@gmail.com>
2022-01-13 07:29:23 -0800

    Fixed error with unique key prop in map function

--- github-users/src/components/GitHubUserList.js
+++ github-users/src/components/GitHubUserList.js
@@ -7,7 +7,7 @@ const GitHubUserList = ({ currentPage, maxPages, setPage, totalResults, users })
   <div className='list-of-users'>
     <div className='result-total'>Total Number of Results: {totalResults}</div>
     {users.map((user, i)  => {
-      return <GitHubUserEntry index={i} userInfo={user} />
+      return <GitHubUserEntry key={i} userInfo={user} />
     })}
     <PageScroller pageNumber={currentPage} maxPages={maxPages} setPage={setPage} />
   </div>
